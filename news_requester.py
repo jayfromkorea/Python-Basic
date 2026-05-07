7e905b61647fd14a577c10c6fb2eb1136220604c
Jay <jayprgacc@gmail.com>
2026-05-07 21:49:03 +0900

    Python Class

--- news_requester.py
+++ news_requester.py
@@ -3,6 +3,8 @@ from typing import Final
 import os
 
 API_KEY:Final = 'd2fd5e1b8b50454aa36d768721b20201'
+BASE_URL:Final = 'https://newsapi.org/v2/top-headlines'
+US:Final = 'us'
 
 '''
 카테고리를 선택 입력받아서 해당 카테고리에 대한 뉴스 기사들을 가져오는 프로그램을 구현하시오
@@ -41,7 +43,11 @@ def menu():
         print('5. Sports')
         print('6. Technology')
         print('===============')
-        choice = int(input('Please choose a category: '))
+        print('x. exit')
+        print('===============')
+        choice = input('Please choose a category: ')
+        if choice.lower() == 'x':
+            return 'x'
         if choice >= 0 and choice <= 6:
             match choice:
                 case 1: return 'Business'
@@ -51,36 +57,51 @@ def menu():
                 case 5: return 'Sports'
                 case 6: return 'Technology'
 
-user_choice = menu()
-print(user_choice)
+#user_choice = menu()
+#print(user_choice)
 
+# 이와 같이 유연하게 바꿀 수 있음
+def make_article_url(country, category):
+    url = f'{BASE_URL}?country={country}&category={category}&apiKey={API_KEY}'
+    return url
 
-res = rq.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=d2fd5e1b8b50454aa36d768721b20201')
-if res.status_code != 200:
-    print('요청한 사이트로부터 데이터를 바을 수 없음')
-    exit(-1)    #프로그램 종료
+def request_artiles(url):
+    res = rq.get(url)
+    if res.status_code != 200:
+        print('요청한 사이트로부터 데이터를 바을 수 없음')
+        exit(-1)    #프로그램 종료
 
-#print(type(res.json()))
-result = res.json()
+    #print(type(res.json()))
+    result = res.json()
 
-if result['status'] != 'ok':
-    print('Invalid Request, Cannot Retrieve data')
-    exit(-1)
+    if result['status'] != 'ok':
+        print('Invalid Request, Cannot Retrieve data')
+        return False
+    
+    os.system('cls')    # 화면 클리어
 
-print(f'전체뉴스 기사 개수: {result["totalResults"]}')
+    print(f'전체뉴스 기사 개수: {result["totalResults"]}')
 
-articles = result["articles"]   # 타입은 list이다
-print(type(articles))
-for article in articles:
-    print(f"제목: {article['title']}")
-    # description
-    # url
-    # author
-    # publishedAt
-    # source.name
-    print(f"Description: {article['description']}")
-    print(f"URL: {article['url']}")
-    print(f"Author: {article['author']}")
-    print(f"Date: {article['publishedAt']}")
-    print(f"Publisher: {article['source']['name']}")
-    print()
+    articles = result["articles"]   # 타입은 list이다
+    print(type(articles))
+    for article in articles:
+        print(f"제목: {article['title']}")
+        # description
+        # url
+        # author
+        # publishedAt
+        # source.name
+        print(f"Description: {article['description']}")
+        print(f"URL: {article['url']}")
+        print(f"Author: {article['author']}")
+        print(f"Date: {article['publishedAt']}")
+        print(f"Publisher: {article['source']['name']}")
+        print()
+
+    return True
+
+if __name__ == '__main__':
+    category = menu()
+    if category != 'x':
+        url = make_article_url(US, category.lower())
+        request_artiles(url)
---
+++ use_class.py
@@ -0,0 +1,140 @@
+
+class Person:
+    country = 'Korea'   # 클래스 멤버 변수
+
+    # 생성자(Constructor): 클래스를 이용하여 인스턴스를 생성하는 아주 특별하 매서드
+    def __init__(self, name, age):
+        self.__age = age    #__age
+        self.name = name  #__name 이와 같이 __를 사용하면 외부에서 바꾸지 말라는 뜻임 컨벤션임
+        # 왜냐하면 파이썬에는 public, private, protected가 없기 때문임
+
+    # class method
+    def change_country(cls, country):
+        cls.country = country
+
+
+    @property
+    def Age(self):  # getter: 외부에 값을 노출하는 기능
+        return self.__age
+    
+    
+    
+    @Age.setter
+    def Age(self, age):
+        if age < 0:
+            return
+        self.__age = age
+
+        # representation
+    def __repr__(self):
+        return f'{self.name}({self.__age})'
+    
+    def __eq__(self, value):
+        if not isinstance(value, Person):
+            return False
+        
+        return self.name == value.name and self.__age == value.__age
+
+    # 클래스 안의 함수들은 Method라고 부름
+    def introduce(self):    # 인스턴스 메서드(Insatnace Method)
+        print(f'안녕하세요, 제 이름은 {self.name}이고 나이는 {self.__age}살입니다')
+
+# 클래스 상속(Inheritance)
+class Student(Person):
+    def __init__(self, name, age):
+        super().__init__(name, age) # 부모 클래스의 생성자를 호출해줌
+        self.grade = 'First Grade'
+        self.total = 0
+        self.avg = 0.0
+
+    # 매서드 재정의(Method Override)
+    def introduce(self):
+        super().introduce()
+        print(f'{self.grade}학년입니다.')
+
+    
+    
+    
+        
+            
+    #def __str__(self):  # 위의 함수와 같은 함수
+    #    return f'{self.name}({self.age})'
+    
+    def exam(self, kor, eng, math):
+        self.total = kor + eng + math
+        self.avg = self.total / 3
+
+    def print_exam(self):
+        print(f'총점: {self.total}, 평균: {self.avg}')
+
+class MyUnit:
+    def __init__(self):
+        self.hp = 100
+        self.attack = 50
+        self.speed = 200
+
+#object: 모든 클래스의 조상 클래스(최상위 클래스)
+
+# Person클래스를 이용하여 hong intance를 생성하다.
+hong = Person('홍박사', 20)
+#num = int(15)
+#name = str('홍길동')
+#hong.age = 20
+#hong.name = '홍박사'
+hong.introduce()
+#print(hong.name, hong.age)
+
+iu = Person('아이유', 30)
+iu.introduce()
+
+iu2 = Person('아이유', 30)
+
+
+lee = Person('이순신', 100)
+lee.introduce()
+
+kim = Student('김민식', 44)
+kim.exam(50, 60, 70)
+kim.print_exam()
+kim.introduce()
+print(kim) #    __repr__
+
+num = 10
+score = 10
+
+if num == score:
+    print('같음')
+
+if iu == iu2:   # iu __eq__(iu2)와 같음
+    print('The same')
+else:
+    print('Different')
+
+
+print(isinstance(num, int))
+print(isinstance(num, str))
+print(isinstance(num, object))
+
+print(isinstance(hong, Person))
+print(isinstance(hong, object))
+print(isinstance(hong, Student))
+print(isinstance(kim, Student))
+print(isinstance(kim, Person))
+print(kim)
+
+hong.__age = 30 # 에러는 발생하지 않음
+hong.introduce()
+hong.Age = 30   # property
+hong.introduce()
+
+Person.country = '대한민국'
+hong.country = 'Korea'
+iu.country = '미국'
+bang = Person('방우식', 67)
+print(hong.country)
+print(bang.country)
+
+Person.change_country(Person, 'Some Country')
+print(Person.country)
+hong.change_country('Taiwan')   # cls는 Person으로 자동 대입
+print(hong.country)
